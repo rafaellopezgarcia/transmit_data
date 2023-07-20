e0cea9eeab45a65bd1dc1772c5d12c43f0664380
rafael.lopez <rafaellopezgarcia27@gmail.com>
2023-07-20 20:45:51 +0900

    feat: add basic project structure

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # transmit_data
+
 Transmit data using protocol buffers.
+
+Objectives
+
+- transmit data from one function to another
+- use protocol buffers as the format to transmit the data
+- serialize and deserialize the data
+- use a class that takes care of the transmission of the data
+- your cmake must have options
+  -- run in gcc and clang
+  -- run in C++14 and C++17
+  -- run in debug and release mode
+  -- install the library
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+template<typename DataType>
+class Transmitter {
+public:
+  void Send(DataType data) {
+    
+  }
+};
+
+template<typename DataType>
+class Receiver {
+public:
+  DataType Receive() {
+
+  }
+};
+
+void ping() {
+  std::cout << "this is ping and sends data to pong" << std::endl;
+  Transmitter<int> transmitter;
+  transmitter.Send(34);
+}
+
+template <typename DataType>
+void pong()  {
+  using namespace std::chrono_literals;
+  std::cout << "this is pong and receives data from ping" << std::endl;
+  while(true) {
+    std::cout << "checking if there is data" << std::endl;
+    std::this_thread::sleep_for(300ms);
+  }
+}
+
+
+int main() {
+  std::cout << "transmit data" << std::endl;
+  std::thread t1(pong<int>);
+  ping();
+  t1.join();
+  return 0;
+}
