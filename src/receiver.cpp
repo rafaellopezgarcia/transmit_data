0115a21578e76568b7e8354401c5e9276c66f112
rafael.lopez <rafaellopezgarcia27@gmail.com>
2023-07-26 22:34:39 +0900

    feat: clang and gcc working together

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -1,5 +1,14 @@
 #include "receiver.hpp"
 
-void Receiver::receive() {
-  std::cout << "ready to receive" << std::endl;
+void Receiver::receive() { 
+  #ifndef NDEBUG
+  std::cout << "DEBUG: ready to receive!" << std::endl;
+  #else 
+  std::cout << "RELEASE: ready to receive" << std::endl;
+  #endif 
+  #ifdef __clang__
+  std::cout << "CLANG: ready to receive" << std::endl;
+  #else
+  std::cout << "GCC: ready to receive" << std::endl;
+  #endif
 }
